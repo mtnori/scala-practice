04b17d9bdf39592c2115b2002185a8811938d8a1
mtnori <moto44.noriyuki@gmail.com>
2019-12-08 16:59:13 +0900

    Create CVS Parser

--- src/main/scala/parser/OrParser.scala
+++ src/main/scala/parser/OrParser.scala
@@ -4,7 +4,7 @@ class OrParser[T](lps: Parser[T], rps: Parser[T]) extends Parser[T] {
   override def parse(input: String): ParseResult[T] = {
     lps.parse(input) match {
       case lResult: ParseOk[T] => ParseOk(lResult.value, lResult.next)
-      case lResult: ParseNg[_] => {
+      case _: ParseNg[_] => {
         rps.parse(input) match {
           case rResult: ParseOk[T] => ParseOk(rResult.value, rResult.next)
           case rResult: ParseNg[_] => ParseNg(rResult.message, rResult.next)
